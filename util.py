f84c1eb106bd667f1079fb22db2e66d5f4b874e6
Geon-woo Lee <gunwoo204@unist.ac.kr>
2022-09-11 07:59:39 +0900

    :sparkles: 데이터 분석 클래스에서 데이터 불러오는 기능 작성

--- util.py
+++ util.py
@@ -1,3 +1,17 @@
+import os
+import pandas as pd
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, 'data/')
+UPLOAD_DIR = os.path.join(DATA_DIR, 'upload/')
+
+
 class FrictionAnalyzer():
-    def __init__(self, filepath: str) -> None:
-        pass
+    def __init__(self, filename: str) -> None:
+        filepath = os.path.join(UPLOAD_DIR, filename)
+        f = pd.read_csv(filepath, index_col=0, header=None)
+        
+        self.raw_friction = f[1]
+        self.raw_load = f[2]
+        self.raw_bimorph = f[3]
